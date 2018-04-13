f89363f1664c29813f0f0aee2ba740b60685c9c3
Lu Xuzheng <466532329@qq.com>
2018-04-13 22:48:16 +0800

    Modify heuristic functions

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -216,8 +216,16 @@ class AirCargoProblem(Problem):
         conditions by ignoring the preconditions required for an action to be
         executed.
         """
-        # TODO implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig Ed-2 11.2)
+        # TODO implement (see Russell-Norvig Ed-3 10.2.3 or Russell-Norvig Ed-2 11.2)
+        # We assume that in each level, at most one goal is completed, so you
+        # only need to increment the counter in 1 for each goals clause that
+        # is not achieved in the current state (not in kb.clauses).
         count = 0
+        kb = PropKB()
+        kb.tell(decode_state(node.state, self.state_map).pos_sentence())
+        for clause in self.goal:
+            if clause not in kb.clauses:
+                count += 1
         return count
 
 
